f8aa3fe448207b634a676447f3905273dfdc924d
Ana Beatriz Lima <77854170+anablima@users.noreply.github.com>
2022-11-28 20:14:31 +0000

    StudyNow

---
+++ FormacaoPython/OO_Python/Cliente.py
@@ -0,0 +1,8 @@
+class Cliente:
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    def get_nome(self):
+        return nome.title()
+    
--- FormacaoPython/OO_Python/Conta.py
+++ FormacaoPython/OO_Python/Conta.py
@@ -2,6 +2,7 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto ... {}".format(self))
+    # Para deixar os atributos privados devemos colocar __ antes
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
@@ -15,3 +16,19 @@ class Conta:
 
     def saca(self, valor):
         self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+    
+    def set_limite(self, limite):
+        self.__limite = limite
