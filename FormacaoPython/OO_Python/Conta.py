80a5f42d798390e32fad4072962579ae1f98152c
Ana Beatriz Lima <77854170+anablima@users.noreply.github.com>
2022-11-28 19:29:47 +0000

    StudyNow

--- FormacaoPython/OO_Python/Conta.py
+++ FormacaoPython/OO_Python/Conta.py
@@ -1,15 +1,17 @@
-from Conta import Conta
-
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto ... {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
-        
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
     def extrato(self):
-        print("Saldo do {} titular {}".format(self.saldo, self.titular))
-        
-        
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
